e1c50a65443a170de0894acd2955ad7aad42ed8c
Pranav Vikraman s s <pranav.vikraman.25@gmail.com>
2026-01-30 12:25:02 +0530

    Update loader.py

--- core/loader.py
+++ core/loader.py
@@ -1,20 +1,9 @@
 import pandas as pd
 
-REQUIRED_COLUMNS = {
-    "S.no",
-    "Main_Component",
-    "Sub_Component",
-    "Preparation_Time",
-    "Activity_Time",
-    "Total_Time",
-    "Manpower"
-}
-
 def load_excel(file):
     df = pd.read_excel(file)
 
-    missing = REQUIRED_COLUMNS - set(df.columns)
-    if missing:
-        raise ValueError(f"Missing columns: {missing}")
+    if df.shape[1] < 7:
+        raise ValueError("Excel must have at least 7 columns")
 
     return df
